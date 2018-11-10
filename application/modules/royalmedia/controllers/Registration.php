e7940f439618a63a6a9f1da40e3bbb09dcbe3caf
samuelw <samuel.wanjohi@nanyukiappfactory.onmicrosoft.com>
2018-11-10 13:21:26 +0300

    user_id added to users table

--- application/modules/royalmedia/controllers/Registration.php
+++ application/modules/royalmedia/controllers/Registration.php
@@ -117,6 +117,6 @@ class Registration extends MX_Controller
         // echo json_encode($response);
         // foreach()
         $sam = json_encode($users);
-        echo $sam[0];
+        echo $sam->id;
     }
 }
