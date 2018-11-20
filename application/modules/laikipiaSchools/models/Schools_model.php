659518c17cca1a8dffe4e894e627168c1f862e5d
nellynkonge <nelly.nkonge@nanyukiappfactory.onmicrosoft.com>
2018-11-20 20:31:25 +0300

    Adding the method to fetch all schools and retun them so as to be mapped by the reactjs app

--- application/modules/laikipiaSchools/models/Schools_model.php
+++ application/modules/laikipiaSchools/models/Schools_model.php
@@ -3,7 +3,7 @@ class Schools_model extends CI_Model
 {
     function fetchschools()
     {
-        $this->db->select('school_name, latitude, longitude, about,logo');
+        $this->db->select('school_name, latitude, longitude, about,logo,boys,girls');
         $this->db->from('laikipia_schools');
         $query = $this->db->get();
         $ans = $query->result();
