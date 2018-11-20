0c6ef172a92c949953f97b0d5ffe779c7fad293a
willis <willis.ivali@nanyukiappfactory.onmicrosoft.com>
2018-11-20 16:24:37 +0300

    query of telesales update

--- application/modules/telesales/controllers/telesales.php
+++ application/modules/telesales/controllers/telesales.php
@@ -51,14 +51,40 @@ class Telesales extends MX_Controller {
 		$json_string = file_get_contents("php://input");
         $json_object = json_decode($json_string);
 		$response = array();
-		var_dump($response);
 		
-		$username = '';
-		$this->db->select('*');
-		$this->db->from('customers');
-		$this->db->where('username', "$username");
-		$customers = $this->db->get();
+		// $username ="0707972693";
+		// if(strpos($username, "254") !== false){
+        //     $username = "+".$username;
+        // }else{
+        //     $username = $username;
+		// }
+		if (is_array($json_object)) {
+            if (count($json_object) > 0) {
+                // foreach ($json_object as $row) {
+				$row = $json_object[0];
+				$username = $row->username;
+				$this->db->select('*');
+				$this->db->from('customers');
+				$this->db->where('username', $username);
+				$customers = $this->db->get();
 
-		echo json_encode($customers->result());
+				echo json_encode($customers->result());
+
+                // }
+            } else {
+                $response["result"] = "false";
+                $response["message"] = "No results present in request object";
+            }
+        } else {
+            $response["result"] = "false";
+            $response["message"] = "Error in request object";
+        }
+		
+		// $this->db->select('*');
+		// $this->db->from('customers');
+		// $this->db->where('username', $username);
+		// $customers = $this->db->get();
+
+		// echo json_encode($customers->result());
     }
 }
